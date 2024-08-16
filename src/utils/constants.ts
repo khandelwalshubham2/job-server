ddb30dbf0ee5507f64c47fb5e8536598d15f622a
khandelwalshubham2 <khandelwalshubham2@gmail.com>
2024-08-16 20:47:26 +0530

    change frontend url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,4 +2,4 @@ export const userRole = ["student", "recruiter"] as const;
 export const applicationStatus = ["pending", "accepted", "rejected"] as const;
 export const localhostFrontendUrl = "http://localhost:5173";
 export const productionFrontEndUrl =
-  "https://mern-job-portal-lm60.onrender.com";
+  "https://mern-job-portal-j4p8.onrender.com/";
